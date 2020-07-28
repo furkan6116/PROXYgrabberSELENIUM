ed11eb62c3b4580474938771dfc32cdfb75cbbd2
Furkan Kamiloğlu <furkank6116@hotmail.com>
2020-07-28 20:00:51 +0300

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 import requests,time
 from threading import Thread
-from proxy_checker import ProxyChecker
 uncheckedproxies = []
 uncheckedproxytypes = []
 checkedproxies = []
 toplambulunan = 0
 kacıncı = 0
-print('\033[92m' + """\
+print("""\
 ___________            __                     ____  __.              .__.__                .__         
 \_   _____/_ _________|  | _______    ____   |    |/ _|____    _____ |__|  |   ____   ____ |  |  __ __ 
  |    __)|  |  \_  __ \  |/ /\__  \  /    \  |      < \__  \  /     \|  |  |  /  _ \ / ___\|  | |  |  
@@ -24,13 +23,13 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-print( bcolors.OKBLUE + "Proxy Grabber For Selenium" + bcolors.ENDC)
-BeklemeMs = input("Kaç Saniyede Bir Yeni Thread Açalım :")
+print("Proxy Grabber For Selenium")
+BeklemeMs = int(input("Kaç Saniyede Bir Yeni Thread Açalım :"))
 
 
 def getallproxy():
     global toplambulunan,uncheckedproxies,uncheckedproxytypes
-    maxdelay = input(bcolors.HEADER + "Proxylerin Yanıt Süresi Maksimum Kaç Milisaniye Olsun : " + bcolors.ENDC)
+    maxdelay = int(input("Proxylerin Yanıt Süresi Maksimum Kaç Milisaniye Olsun : "))
     types = ["socks4","socks5","http"]
     for ttype in types:
         for asd in requests.get("https://api.proxyscrape.com/?request=getproxies&proxytype=" + ttype +"&timeout=" + str(
@@ -38,7 +37,7 @@ def getallproxy():
             toplambulunan += 1
             uncheckedproxies.append(str(asd)[2:-1])
             uncheckedproxytypes.append(ttype)
-    print(bcolors.HEADER + str(toplambulunan) + " Proxy Bulundu" + bcolors.ENDC)
+    print(str(toplambulunan) + " Proxy Bulundu")
 
 
 def proxydene(proxy,önek,kacinci):
@@ -47,25 +46,25 @@ def proxydene(proxy,önek,kacinci):
     options.add_argument("--incognito")
     options.add_argument("--headless")
     options.add_argument("--proxy-server="+ önek +"://" + proxy)
-    options.add_argument("log-level=2")
+    options.add_argument("log-level=3")
     chrome = webdriver.Chrome(options=options)
     chrome.get("https://api.ipify.org")
     time.sleep(0.5)
     try:
         chrome.find_element_by_xpath("/html/body/pre").text
-        print(bcolors.OKGREEN + "Çalışıyor " + proxy + " (" + str(kacinci + 1) + "/" + str(
-            toplambulunan) + ")" + bcolors.ENDC)
+        print("Çalışıyor " + proxy + " (" + str(kacinci + 1) + "/" + str(
+            toplambulunan) + ")")
         checkedproxies.append("--proxy-server=" + önek + "://" + proxy)
         open("çalışanproxyler.txt","a").write("--proxy-server=" + önek + "://" + proxy + "\n")
         print(str(len(checkedproxies)) + " Proxy Düzgün Çalışıyor.")
     except:
-        print(bcolors.FAIL + "Bozuk " + proxy +" (" + str(kacinci + 1) + "/" + str(toplambulunan) + ")" + bcolors.ENDC)
+        print("Bozuk " + proxy +" (" + str(kacinci + 1) + "/" + str(toplambulunan) + ")")
     chrome.close()
     chrome.quit()
     return
 
 getallproxy()
-print(bcolors.WARNING + "Proxyler Deneniyor" + bcolors.ENDC)
+print("Proxyler Deneniyor")
 for proxy in uncheckedproxies:
     if __name__ == "__main__":
         Thread(target=proxydene, args=(proxy, uncheckedproxytypes[kacıncı], kacıncı)).start()
